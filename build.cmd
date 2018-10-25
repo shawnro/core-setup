5779371f24efe8cdfe9c45eea6a2a16432979132
Shawn Rothlisberger <shawnr@microsoft.com>
2018-10-25 14:14:47 -0700

    Bare bones

--- build.cmd
+++ build.cmd
@@ -1,2 +1,11 @@
-@call %~dp0run.cmd build %*
-@exit /b %ERRORLEVEL%
+REM @call %~dp0run.cmd build %*
+REM @exit /b %ERRORLEVEL%
+
+@echo off
+REM TODO: this environment variable is set for this machine where ref assemblies were copied by hand
+
+set TargetFrameworkRootPath=D:\temp\refassem
+
+call eng\common\init-tools-native.cmd
+powershell -ExecutionPolicy ByPass -NoProfile -command "& """%~dp0eng\common\Build.ps1""" -restore -build %*"
+exit /b %ErrorLevel%
